00768d7ab400565ee3b9e511758107ccc8b36072
Akira TANIDA <akira@MPB16.local>
2022-11-11 10:44:27 +0900

    refactor strnstrl; change to SEGV on condition 'haystack=NULL & len!=0'

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -17,12 +17,14 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	i;
 	size_t	needle_len;
 
+	i = 0;
 	needle_len = ft_strlen(needle);
 	if (needle_len == 0)
 		return ((char *)haystack);
-	i = 0;
-	while ((i + needle_len <= len) && (haystack[i] != '\0'))
+	while (i < len && haystack[i] != '\0')
 	{
+		if (len - i < needle_len)
+			return (NULL);
 		if (ft_strncmp(&haystack[i], needle, needle_len) == 0)
 			return ((char *)&haystack[i]);
 		i++;
