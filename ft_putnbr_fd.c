8561e9e9f8b69f79aa4c9e1269b031caac928d7c
Akira TANIDA <akira@MPB16.local>
2022-11-13 19:11:46 +0900

    refactor logic for ft_putnbr_fd; change to use recursiong

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,40 +6,30 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 10:36:57 by takira            #+#    #+#             */
-/*   Updated: 2022/10/17 10:36:58 by takira           ###   ########.fr       */
+/*   Updated: 2022/11/13 19:01:11 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static long long	get_pow(long long num)
+static void	recursion_put_nbr(long long num, int fd)
 {
-	long long	pow;
-
 	if (num == 0)
-		return (1);
-	pow = 1;
-	while (num / pow > 0)
-		pow *= 10;
-	return (pow / 10);
+		return ;
+	recursion_put_nbr(num / 10, fd);
+	ft_putchar_fd(num % 10 + '0', fd);
 }
 
 void	ft_putnbr_fd(int n, int fd)
 {
 	long long	num;
-	long long	pow;
 
-	num = (long long)n;
-	if (num < 0)
+	if (n >= 0)
+		num = n;
+	else
 	{
+		num = -n;
 		ft_putchar_fd('-', fd);
-		num *= (-1);
 	}
-	pow = get_pow(num);
-	while (pow > 0)
-	{
-		ft_putchar_fd((char)(num / pow + '0'), fd);
-		num %= pow;
-		pow /= 10;
-	}
+	recursion_put_nbr(num, fd);
 }
